631cd42eb9ae833b7a2ca9889ee6c2b5ac288988
Lars Wiedemann <lars.wiedemamnn@vivamera.com>
2017-09-29 08:03:20 +0200

    customize server

--- src/server/main.js
+++ src/server/main.js
@@ -10,27 +10,31 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import swapiSchema from '../schema';
 
+const cors = require('cors');
+
 const app = express();
+app.set('port', 3000);
 
 // Requests to /graphql redirect to /
 app.all('/graphql', (req, res) => res.redirect('/'));
-
 app.use(
-  '/',
-  graphqlHTTP(() => ({
-    schema: swapiSchema,
-    graphiql: true,
-  })),
+    '/'
+    , cors()
+    ,
+    graphqlHTTP(() => ({
+        schema: swapiSchema,
+        graphiql: true,
+    })),
 );
 
 // Listen for incoming HTTP requests
-const listener = app.listen(() => {
-  let host = listener.address().address;
-  if (host === '::') {
-    host = 'localhost';
-  }
-  const port = listener.address().port;
-  /* eslint-disable no-console */
-  console.log('Listening at http://%s%s', host, port === 80 ? '' : ':' + port);
-  /* eslint-enable no-console */
+const listener = app.listen(3000, () => {
+    let host = listener.address().address;
+    if (host === '::') {
+        host = 'localhost';
+    }
+    const port = listener.address().port;
+    /* eslint-disable no-console */
+    console.log('Listening at http://%s%s', host, port === 80 ? '' : ':' + port);
+    /* eslint-enable no-console */
 });
